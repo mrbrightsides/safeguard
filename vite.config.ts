0d16cdb92d1fea7304fa4b7500f13c9c7efacaaf
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2026-04-06 12:21:19 +0700

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: './', // Ubah dari '/' ke './' untuk relative path
+    base: '/', // Ubah dari '/' ke './' untuk relative path
     define: {
       'process.env.VITE_GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
     },
